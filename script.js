474b4dde2655af061ce5effcde9fbd4b198930ac
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-22 01:11:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+var section = document.getElementById("buttons")
+// alert(section)
+section.addEventListener('click', function(e) {
+    if(e.target.className === 'btn') {
+        var soundName = `/sounds/applause.mp3`;
+		// alert(soundName);
+		
+		const audio = new Audio(soundName);
+		audio.play();
+		
+    }
+	// alert(e.target.className)
+});
